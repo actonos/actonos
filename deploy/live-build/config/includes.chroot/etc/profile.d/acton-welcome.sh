0c7cd6a3cc620c9006008d292bf96c322c34533b
Bieber Kieu <kieutrongthien@gmail.com>
2026-08-19 23:38:08 +0700

    fix(live-build): freeze

--- deploy/live-build/config/includes.chroot/etc/profile.d/acton-welcome.sh
+++ deploy/live-build/config/includes.chroot/etc/profile.d/acton-welcome.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-# Auto-run ActonOS welcome banner on interactive login on tty1
+# Auto-run ActonOS interactive installer & setup wizard on tty1
 if [ "$(tty)" = "/dev/tty1" ]; then
-    if [ -x /usr/local/bin/acton-welcome ]; then
+    if [ -x /usr/local/bin/acton-install-disk ]; then
+        /usr/local/bin/acton-install-disk
+    elif [ -x /usr/local/bin/acton-welcome ]; then
         /usr/local/bin/acton-welcome
     fi
 fi
